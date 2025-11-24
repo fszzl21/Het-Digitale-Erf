9464673f257a97b87115fd3696095f19224991cb
ToxominApe <99971095+ToxominApe@users.noreply.github.com>
2025-11-24 13:33:02 +0100

    Javascript beginsel
    Proberen om een html element te creëren met javascript, nog niet gelukt.

---
+++ Agenda/HTML/agendaMaand.js
@@ -0,0 +1,12 @@
+/*function elementFromHTML(html) {
+  const template = document.createElement("template")
+
+  template.innerHTML = html.trim()
+
+  return template.content.firstElementChild;
+}
+
+const myList = elementFromHTML("
+  <ul>  <li>dcode</li> </ul>");
+
+document.getElementById("agendaGrid").appendChild(myList);*/
